01825d2f6c77f6bd89425d977d1f49a14fdbe688
Ethan <kylsia2008@gmail.com>
2026-09-14 19:17:40 +0100

    create tracking for tokens including Current and Next methods. Build upon the lexer functionality.

--- src/main.cpp
+++ src/main.cpp
@@ -30,13 +30,43 @@ class Lexer{
   private:
     const std::string _text; // read only
     int _position;
+    char Current() {
+        if (_position >= _text.length()) {
+            return '\0'; // return null if out of bounds
+        }
+        return _text[_position]; // return current character
+    }
+    void Next(){
+        _position++;
+    }
+
   public:
     Lexer(const std::string& text) : _text(text) {}
 
     std::string getText() const {
         return _text;
     }
+    
+    SyntaxToken NextToken(){
+        // numbers, operators, whitespace.
+        if(std::isdigit(Current())){
+            auto start = _position;
+            while(isdigit(Current())){
+                Next();
+            }
 
+            auto length = _position - start;
+            auto text = _text.substr(start, length);
+            
+            return SyntaxToken(SyntaxKind::NumberToken, start, text, std::stoi(text));
+
+
+        }
+        return SyntaxToken(SyntaxKind::BadToken, _position, "", std::any());
+
+        
+        
+    }
   
 };
 
@@ -47,3 +77,4 @@ bool isNullOrWhiteSpace(const std::string& str) {
    
     return std::all_of(str.begin(), str.end(), [](unsigned char c) { return std::isspace(c); });
 }
+
